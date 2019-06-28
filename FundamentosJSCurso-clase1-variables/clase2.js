28c4f839a8cc9b3d53ea6fd6c816e8e6b03e676a
volta <giovoltadesign@gmail.com>
2019-06-28 18:07:41 -0400

    initial commit

---
+++ FundamentosJSCurso-clase1-variables/clase2.js
@@ -0,0 +1,18 @@
+var nombre = 'Sacha', apellido = 'Lifszyc'
+
+var nombreEnMayusculas = nombre.toUpperCase()
+var apellidoEnMinusculas = apellido.toLowerCase()
+
+var primeraLetraDelNombre = nombre.charAt(0)
+var cantidadDeLetrasDelNombre = nombre.length
+
+// var nombreEnMayusculas = nombre.toUpperCase()
+// var apellidoEnMinusculas = apellido.toLowerCase()
+
+// var primeraLetraDelNombre = nombre.charAt(0)
+// var cantidadDeLetrasDelNombre = nombre.length
+var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`
+var str = nombre.charAt(1) + nombreCompleto.charAt(2)
+
+
+var str = nombre.substr(1, 2)
