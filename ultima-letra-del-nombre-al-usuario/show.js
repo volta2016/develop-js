7eab673fd6d5629b2e4f9d79f35812b3849a7d71
volta <giovoltadesign@gmail.com>
2019-06-29 17:38:51 -0400

    cambios show js

--- ultima-letra-del-nombre-al-usuario/show.js
+++ ultima-letra-del-nombre-al-usuario/show.js
@@ -6,6 +6,7 @@ var nombre_b = "dani";
 var ultimaLetra = nombre_b.charAt(nombre.length-1)
 
 function show() {
-	alert("la última letra de su nombre es:" + nombre.value.charAt(nombre.length - 1));
-	alert("la útima letra de su apellido es:" + apellido.value.charAt(apellido.length - 1));
+	alert("la última letra de su nombre es:" + nombre.value.charAt(nombre.value.length - 1));
+	alert("la útima letra de su apellido es:" + apellido.value.charAt(apellido.value.length - 1));
+	console.log(nombre_b);
 }
