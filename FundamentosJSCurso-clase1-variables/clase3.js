97fcb34e13bd9a7a79f01ba6af70a0f1e11d850d
volta <giovoltadesign@gmail.com>
2019-06-30 17:46:26 -0400

    cambios html y js clas3

---
+++ FundamentosJSCurso-clase1-variables/clase3.js
@@ -0,0 +1,19 @@
+var edad = 27;
+//edad = edad + 1;
+edad += 1;
+var peso = 75;
+//var peso = peso - 2;
+peso -= 2;
+var sandwich = 1;
+peso += sandwich;
+var jugarAlFutbol = 3;
+peso -= jugarAlFutbol;
+
+var precioDeVino = 200.3
+var total = Math.round(precioDeVino * 100 * 3) / 100;
+var totalStr = total.toFixed(2); 
+var total2 = parseFloat(totalStr);
+
+var pizza = 8;
+var personas = 2;
+var catidadDePorcionesPorPersonas = pizza / personas;
