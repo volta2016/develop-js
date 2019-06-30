7dad63ad34f77f3f2336564625e885fe2d375ba6
volta <giovoltadesign@gmail.com>
2019-06-30 13:55:07 -0400

    cambios js substr

--- ultima-letra-del-nombre-al-usuario/show.js
+++ ultima-letra-del-nombre-al-usuario/show.js
@@ -4,6 +4,8 @@ var boton = document.getElementById("boton");
 boton.addEventListener("click", show);
 var nombre_b = "dani";
 var ultimaLetra = nombre_b.charAt(nombre_b.length - 1);
+var nombre_c = "Joiler";
+var str = nombre_c.substr(-1);
 
 function show() {
 	alert("la última letra de su nombre es:" + nombre.value.charAt(nombre.value.length - 1));
