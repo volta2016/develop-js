de15c6c45c5fffc3561279f330499b76e85f20ca
volta <giovoltadesign@gmail.com>
2019-07-02 12:54:57 -0400

    js 5

--- FundamentosJSCurso-clase1-variables/clase5.js
+++ FundamentosJSCurso-clase1-variables/clase5.js
@@ -9,16 +9,23 @@ imprimirNombreEnMayuscula(nombre)
 
 //nuevo entorno
 var nombreMusica = 'JAZZ'
+//var global 
 
+//funcion + parametro n local
 function imprimirMusicaMinuscula(nombreMusica) {
+	nombreMusica.toLowerCase()
 	var minus = nombreMusica.toLowerCase()
 	console.log(`en minuscula ${minus}`)
 	console.log(`impresion de nombre local dentro de la funcion ${nombreMusica}`)
 	console.log(`impresion de nombre Global dentro de la funcion ${window.nombreMusica}`)
 	console.log(`Su ultima letra es ${nombreMusica.charAt(3)}`)
 	console.log(`su primeras 2 letras ${nombreMusica.substr(0,2)}`)
-
+	// lo que hace nombreMusica dentro de par existe en alcance local 
+	// de la funcion sin importar el alncance global.
+	//la var nombreMusica n global lo vamos a poder acceder dentro de la funcion 
+	//excepto que nos refiramos explicitamente a window.nombre que es a n global
+	//el obejeto en el navegador es window
 	//forma de concatenar con template literals
 } 
 imprimirMusicaMinuscula(nombreMusica)
-console.log(`impresion fuera de la funcion` + nombreMusica)
+console.log(`impresion fuera de la funcion ${nombreMusica}`)
