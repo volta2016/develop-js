7a3dd49360df7ea0e59fd305b506d66a85218062
volta <giovoltadesign@gmail.com>
2019-07-04 11:21:33 -0400

    js6 cambiosb

--- FundamentosJSCurso-clase1-variables/clase6.js
+++ FundamentosJSCurso-clase1-variables/clase6.js
@@ -2,7 +2,6 @@ var sasuke = {
 	nombre : 'Sasuke',
 	apelido : 'Uchiha',
 	edad : 19
-
 }
 var itachi = {
 	nombre : 'Itachi',
@@ -14,12 +13,20 @@ function imprimirNombreEnMayuscula({ nombre }) {
 	console.log(nombre.toUpperCase())
 }
 
+// function imprimirNombreEnMayuscula(persona) {
+// 	console.log(persona.nombre.toUpperCase())
+// }
+//Acá podemos ver que la función sabe que tiene que acceder 
+//al atributo nombre Abajo imprimimos la función 
+
+
 imprimirNombreEnMayuscula(sasuke)
+//imprime la funcion en consola
 
 imprimirNombreEnMayuscula(itachi)
 //poemos poner un objeto acá mismo
-//imprimirNombreEnMayuscula({nombre: 'Kakashi'})
-imprimirNombreEnMayuscula({apellido: 'Gomez'})
+imprimirNombreEnMayuscula({nombre: 'Kakashi'})
+// imprimirNombreEnMayuscula({apellido: 'Gomez'})
 //no va correr por el objeto que le pasamos 
 //no tiene el atributo nombre
 
