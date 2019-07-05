9ba5c8821bfe1f986f0332eb68b9e5f6c5fe654e
volta <giovoltadesign@gmail.com>
2019-07-05 19:59:10 -0400

    new example value edad conditions

--- FundamentosJSCurso-clase1-variables/clase10.js
+++ FundamentosJSCurso-clase1-variables/clase10.js
@@ -1,8 +1,8 @@
 var kyo = {
-	nombre : 'kyo',
+	nombre : 'Kyo',
 	apellido : 'Ito',
-	edad : 19
-	igeniero: true,
+	edad : 19,
+	ingeniero: false,
 	cocinero: false,
 	cantante: false,
 	dj: false,
@@ -13,7 +13,34 @@ var kyo = {
 function imprimirProfesiones(persona) {
 	console.log(`${persona.nombre} es: ` )
 	//aplico template string
-	if (persona.ingeniero === true){
-		console.log('ingeniero')
+	if (persona.ingeniero){
+		console.log('Ingeniero')
+	} else {
+		console.log('No es ingeniero')
+	}
+	if (persona.cocinero){
+		console.log('Cocinero')
+	}
+	if (persona.cantante){
+		console.log('Cantante')
+	}
+	if (persona.dj){
+		console.log('Dj')
+	}
+	if(persona.guitarrista){
+		console.log('Guitarrista')
+	}
+	if (persona.drone) {
+		console.log('Piloto de drone')
 	}
 }
+function imprimirSiEsMayorDeEdad(persona){
+	if (persona.edad >= 18 ){
+		console.log('Kyo mayor de edad')
+	} else {
+		console.log('Kyo Eres menor de edad')
+	}
+
+}
+imprimirProfesiones(kyo)
+imprimirSiEsMayorDeEdad(kyo)
