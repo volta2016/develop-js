72387067feb0951880bf967273012c62dc03a45f
volta <giovoltadesign@gmail.com>
2019-07-05 10:04:29 -0400

    js9 and file

---
+++ FundamentosJSCurso-clase1-variables/clase9.js
@@ -0,0 +1,12 @@
+var x = 4, y = '4'
+
+var kyo = {
+	nombre: 'kyo'
+}
+
+var otraPersona = {
+	nombre: 'Kyo'
+
+}
+//como podemos observar los objetos estos son iguales pero
+//cada uno esta en una variable distinta
