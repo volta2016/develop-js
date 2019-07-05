1ad9916d708c9914ee21e76725adfb1f276bc119
volta <giovoltadesign@gmail.com>
2019-07-05 14:25:39 -0400

    cambios 9js

--- FundamentosJSCurso-clase1-variables/clase9.js
+++ FundamentosJSCurso-clase1-variables/clase9.js
@@ -4,9 +4,40 @@ var kyo = {
 	nombre: 'kyo'
 }
 
+
+// var otraPersona = kyo
+// aca corre como el mismo objeto. otraPersona y kyo apuntan al
+// mismo lugar entonces se va modificar en los 2
+
+
 var otraPersona = {
-	nombre: 'Kyo'
+	nombre: 'kyo'
+}
+// tienen los 2 el atributo nombre con el valor kyo  pero son 
+//diferentes objetos es otra posicion en memoria ram , 
+//si lo hacemos como estaba antes 
+
+//modificamos otra persona, tienen los mismos valores pero apuntan a 
+//a diferentes direcciones en momoria ram kyo se sigue llamando kyo
+// y otraPersona el valor que midifcamos que fue este otraPersona.nombre = 'Pepe'
+//cambia kyo por Pepe
+
+
+var jugador = {
+
+nombre: 'riot',
+edad: 20
 
 }
-//como podemos observar los objetos estos son iguales pero
-//cada uno esta en una variable distinta
+// var jugador2 = jugador
+var jugador2= {
+
+    nombre: 'riot',
+    edad: 20
+}
+
+if (jugador === jugador2) {
+    console.log('Verdadero')
+} else {
+    console.log('Falso')
+}
