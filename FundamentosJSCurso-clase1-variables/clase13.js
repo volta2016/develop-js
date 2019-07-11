871a5b99aa1964933491c3e4a5f05716dcb84e0c
volta <giovoltadesign@gmail.com>
2019-07-11 16:35:25 -0400

    js13-1

--- FundamentosJSCurso-clase1-variables/clase13.js
+++ FundamentosJSCurso-clase1-variables/clase13.js
@@ -6,15 +6,35 @@ var kyo = {
 }
 console.log(`Al inicio del año ${kyo.nombre} pesa ${kyo.peso}kg`)
 
-for (var i = 1; i <= 365; i++) {
+//aumentar de peso va a ser una una función, los parametros van entre parentesis que va 
+//a recibir una persona y un incremento => y lo que va hacer es 
+//persona punto peso += incremento.
+
+// function aumentarDePeso (persona) {
+// 	return persona.peso +=200
+// }
+// el arrow function es lo misma funcion que esta arriba
+const INCREMENTO_PESO = 0.2
+const DIAS_DEL_ANO = 365
+
+const aumentarDePeso = (persona) => persona.peso += INCREMENTO_PESO
+const adelgazarDePeso = (persona) => persona.peso -= INCREMENTO_PESO
+// como vimos anteriormente 200 son magic number no sabemos bn de donde salen.
+// entonces vamos a ponerlos en una constante.
+
+
+
+for (var i = 1; i <= DIAS_DEL_ANO; i++) {
 	var random = Math.random()
 	if (random < 0.25) {
-		// aumentoDePeso
+		aumentarDePeso(kyo)
 	} else if (random < 0.5){
-		// entoncesVaAdelgazar
+		adelgazarDePeso(kyo)
 	}
-
 }
+// recordemos como kyo acá arriba es un objeto ese objeto se va a ver moodificado fuera
+//de la opcion también si no queremos que eso ocurra tenemos que deglozar el objeto original
+//también
 
-console.log(`Al final del año ${kyo.nombre} pesa ${kyo.peso}kg`)
+console.log(`Al final del año ${kyo.nombre} pesa ${kyo.peso.toFixed(2)}kg`)
 
