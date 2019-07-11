7fd8430c5d547259e0397766323b856659155993
volta <giovoltadesign@gmail.com>
2019-07-11 12:19:04 -0400

    js 13

---
+++ FundamentosJSCurso-clase1-variables/clase13.js
@@ -0,0 +1,10 @@
+var kyo = {
+	nombre : 'Kyo',
+	apellido : 'Ito',
+	edad : 28,
+	peso : 75
+}
+console.log(`Al inicio del año ${kyo.nombre} pesa ${kyo.peso}kg`)
+
+console.log(`Al final del año ${kyo.nombre} pesa ${kyo.peso}kg`)
+
