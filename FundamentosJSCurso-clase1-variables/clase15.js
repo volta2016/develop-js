a6701eaf1eb6dcee030206c9b7ca890f8f4b0d0a
volta <giovoltadesign@gmail.com>
2019-07-17 22:29:41 -0400

    js 15

--- FundamentosJSCurso-clase1-variables/clase15.js
+++ FundamentosJSCurso-clase1-variables/clase15.js
@@ -1,26 +1,38 @@
 var contador = 0 
-
-const llueve = () => Math.random() < 0.25
-
-//vamos hacer algo alguna una vez hasta que se cumpla la condicion 
-// si este de código de abajo que es el while se cumple este códogo
+//arrow function
+const llueve = () => Math.random() < 0.25 //25%
+//funcion normal
+// const llueve = function () {
+//   returnMath.random() < 0.25;
+// }
+//vamos hacer algo almenos una vez hasta que se cumpla una condicion 
+// si este de código de abajo que es el while se cumple este código
 //se vuelve a ejecutar por que almenos se ejecuta una vez 
 do {
 	contador++
 } while (!llueve())
 
-if (contador === 1) {
-	console.log(`fui a ver si llovia ${contador}vez`)
+// if (contador === 1) {
+// 	console.log(`fui a ver si llovia ${contador}vez`)
+// } else {
+// 	console.log(`fui a ver si llovia ${contador}veces`)
+// }
+//otra forma
+if(contador != 1){
+    console.log(`fui a ver si llovía ${contador} veces`)
+    
 } else {
-	console.log(`fui a ver si llovia ${contador}veces`)
+    console.log(`fui a ver si llovía ${contador} vez`)
+
 }
-
-
+contador === 1 ? console.log(`Fui a ver si llovia ${contador}vez`) : console.log(`Fui a ver si llovia ${contador}veces`)
+//verificar code
+console.log(`Fui a ver si llovía ${contador}${contador > 1 ? 'veces' : 'vez'}`)
 
 // el do donde se encuentra contador++ 
 // esto se va a repetir mientras llueva.
 // entonces acá podemos negar la condicion de que !llueve
-// si no llueve do se va ejecutar de nuevo, si no llueve
+// si "no" llueve do se va ejecutar de nuevo, si no llueve
 // va dejar de ejecutar y va entrar el programa en la linea
 // 11
 
