65f17436859ea615af699cb557c1fcfd90ec676d
volta <giovoltadesign@gmail.com>
2019-07-25 11:57:48 -0400

    js 3

--- FundamentosJSCurso-clase1-variables/clase3.js
+++ FundamentosJSCurso-clase1-variables/clase3.js
@@ -3,7 +3,8 @@ var edad = 27;
 edad += 1;
 var peso = 75;
 //var peso = peso - 2;
-peso -= 2;
+ 
+// var res = peso -= 2;
 var sandwich = 1;
 peso += sandwich;
 var jugarAlFutbol = 3;
@@ -17,3 +18,21 @@ var total2 = parseFloat(totalStr);
 var pizza = 8;
 var personas = 2;
 var catidadDePorcionesPorPersonas = pizza / personas;
+console.log(peso)
+
+//tenemos que tener en cuenta siempre que hagamos 
+//funciones que trabajen con decimales la manera de almacenar decimales no están precisa
+//entonces no termina de ser tan preciso.
+//Para sobrepasar este problema podemos aplicar esto
+//var total = precioDeVino * 100 * 3 / 100;
+
+//var totalStr = total.toFixed(2); 
+//es decir siempre ten en cuenta 2 decimales después de la coma. Le aplicamos a la var total
+
+//var total2 = parseFloat(totalStr)
+//Si lo queremos pasar de string a numero entero se debe aplicar. 
+
+//También puedes utilizar otros operadores acompañados del signo de asignación (=):
+//‘edad *= 2’ Multiplicación
+//’edad **= 2’ Potencia
+//’edad /= 2’ División
