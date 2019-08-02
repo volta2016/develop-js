be0fe3bd385ad947f65938052b83d88a3be5645e
volta <giovoltadesign@gmail.com>
2019-08-02 18:07:04 -0400

    js 24 22

--- FundamentosJSCurso-clase1-variables/clase24-verdad-oculta-sobre-class.js
+++ FundamentosJSCurso-clase1-variables/clase24-verdad-oculta-sobre-class.js
@@ -4,6 +4,25 @@
 //::en otro lenguaje se conoce como constructor
 //entonces esta funcion se va crear cuado creemos una persona
 
+
+function heredaDe(protipoHijo, protipoPadre) {
+    var fn = function () {}
+    fn.prototype = protipoPadre.prototype
+    protipoHijo.prototype = new fn
+    protipoHijo.prototype.constructor = protipoHijo //si no agregabamos a esta linea se
+    //va estar llamando al constructor de protipo padre.
+}
+//lo que tenemos que hacer es decirle  al protipo hijo quien va a ser su padre,
+//basicamente lo que tenemos que hacer asignarle un prototipo este protipo "clase"
+//hija
+//var con funcion anonima
+//a continuación lo que vamos hacer es al prototipo hijo , e vamos a decir que su 
+//prototype es un nuevo objeto de la funcion que creamos acá
+//basicamente estamos haciendo lo mismo pero para no estar pisando el prototipe del
+//protipoPadre lo pasamos a otro objeto.
+//::ademas lo que tenemos qu hacer es asignar la funcion constructura
+//la funcion contructora de la clase hija despues de new fn.
+
 //___nuestro constructor o prototipo____
 function Persona(nombre, apellido, altura) {
     this.nombre = nombre
@@ -64,15 +83,37 @@ Persona.prototype.soyAlto = function() {
   return this.altura > 1.8
 }
 //esta condicion nunca se va dar por que es objeto window
+//lo que quermos hacer un nuevo prototype que sea de desarrollador
+//"un sub tipo de persona"
 
+//luego de que todo lo que hicimos con el prototipo de persona
+//esta funcion va a ser cuando la funcion que se va ejecutar cuando creemos 
+//nuevos tipos de desarroladores
+function Desarrollador (nombre, apellido) {
+    this.nombre = nombre
+    this.apellido = apellido
+
+}
+
+//vamos a pisar la funcion que tenia la persona
+//entonces el saludo de los desarrolladores va a ser distinto 
+Desarrollador.prototype.saludar = function () {
+    console.log(`hola me llamo ${this.nombre} ${this.apellido} y soy desarrolador/a`)
+}
+//lo que nosotros queremos hacer ante de empezar a utilizar nuestros objetos
+//y crear objetos de nuestros prototipos
+//vamos a querer llamar esta funcion - primero la clase hiha y luego la clase padre
+//y ahora si vamos a definir la funcion hereda de
+
+heredaDe(Desarrollador, Persona)
 
 var soyAlto = persona => persona.altura >= 1.80
 
-var sacha = new Persona('Sacha', 'lifszyc', 1.72)
-var erika = new Persona('Erika', 'Luna', 1.65)
-var arturo = new Persona('Arturo', 'Martinez', 1.90)
-arturo.soyAlto()
-arturo.saludar()
+// var sacha = new Persona('Sacha', 'lifszyc', 1.72)
+// var erika = new Persona('Erika', 'Luna', 1.65)
+// var arturo = new Persona('Arturo', 'Martinez', 1.90)
+// arturo.soyAlto()
+// arturo.saludar()
 
 //__orden:
 //function 
