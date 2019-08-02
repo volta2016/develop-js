29d0d2735429011b586450fc8f20fd7e819ad43d
volta <giovoltadesign@gmail.com>
2019-08-02 14:27:05 -0400

    js 22

--- FundamentosJSCurso-clase1-variables/clase22-modificando-prototipo.js
+++ FundamentosJSCurso-clase1-variables/clase22-modificando-prototipo.js
@@ -1,8 +1,10 @@
-//vamos a definir un prototipo que va a definir el protipo 
+//vamos a definir un funcion que va a definir el protipo 
 //para definir un prototipo lo unico que tenemos que definir es una funcion 
 //::esta funcion es la que se va a ejecutar cuando creemos una nueva persona, es como
 //::en otro lenguaje se conoce como constructor
-//entonces esta funcion de va crear cuado creemos una persona
+//entonces esta funcion se va crear cuado creemos una persona
+
+//___nuestro constructor o prototipo____
 function Persona(nombre, apellido, altura) {
     this.nombre = nombre
     this.apellido = apellido
@@ -14,30 +16,46 @@ function Persona(nombre, apellido, altura) {
 //nombre y apellido por ejemplo
 
 //y para guardarlo dentro de este objeto que se esta construyendo en la memoria
-//podemos hacer referencia a ese objeto dentro de esta funcion como como this
+//podemos hacer referencia a ese objeto dentro de esta funcion como como this.
 //this va hacer referencia a ese objeto que se acaba de contruir
 //entonces this.nombre le vamos a asignar nombre
-Persona.prototype.soyAlto = function () {
-    if (soyAlto(this)) {
-        console.log(`hola, soy ${this.nombre} ${this.apellido}, soy alto`)
-    } else {
-        console.log(`Hola soy ${this.nombre} ${this.apellido}, no soy alto`)
-    }
-    // console.log(`Hola, me llamo ${this.nombre} ${this.apellido} `)
-}
-
+// Persona.prototype.soyAlto = function () {
+//     if (soyAlto(this)) {
+//         console.log(`hola, soy ${this.nombre} ${this.apellido}, soy alto`)
+//     } else {
+//         console.log(`Hola soy ${this.nombre} ${this.apellido}, no soy alto`)
+//     }
+//     // console.log(`Hola, me llamo ${this.nombre} ${this.apellido} `)
+// }
+//___por ejemplo para agregar una nueva funcion que ejecute en navegador debemos agregar
+//a nuestro constructor persona.prototype.saludar  
 Persona.prototype.saludar = function() {
-    console.log(`hola me llamos ${this.nombre} ${this.apellido}`)
+    console.log(`hola me llamo: ${this.nombre} ${this.apellido}`)
 }
 
-// ::applied forma  teacher::
+// ::applied form  teacher::
 //lo que tenemos que entender es que el prototipo es un objeto mas dentro de js
 //si lo modificamos, en una cierta altura del codigo y partir de ahi va quedar 
-//modifica 
+//modificado 
+
+// Persona.prototype.soyAlto = function () {
+//      return this.altura > 1.8
+//  }
+//para transformar en arrow sacamos function aplicams () sacamos {}
+//y borramos return
 
 // la aplicamos como arrow function
 
-Persona.prototype.soyAlto = function =>  return this.altuta
+// Persona.prototype.soyAlto = () =>  this.altura > 1.8
+//dejamos el codigo hasta con un bug lo unico que hemos hecho fue pasar una funcion 
+//a arrow function.
+//que pasa con las arrow function ? que pasa con el this dentro de ellas?
+//vamos a volverlo a estado anterior y aplicamos un debugger
+Persona.prototype.soyAlto = () => {
+  debugger
+  return this.altura > 1.8
+}
+//esta condicion nunca se va dar por que es objeto window
 
 
 var soyAlto = persona => persona.altura >= 1.80
@@ -47,4 +65,9 @@ var erika = new Persona('Erika', 'Luna', 1.65)
 var arturo = new Persona('Arturo', 'Martinez', 1.90)
 arturo.soyAlto()
 arturo.saludar()
+
+//__orden:
+//function 
+//var
+//function()invocar
 
