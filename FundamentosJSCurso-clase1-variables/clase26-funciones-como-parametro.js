799dafb782bec7c37fb4a5064d52f0aea877874a
volta <giovoltadesign@gmail.com>
2019-08-05 17:12:54 -0400

    js 26

--- FundamentosJSCurso-clase1-variables/clase26-funciones-como-parametro.js
+++ FundamentosJSCurso-clase1-variables/clase26-funciones-como-parametro.js
@@ -9,13 +9,37 @@ class Persona {
         this.apellido = apellido
         this.altura = altura
     }
-    saludar() {
-        console.log(`hola me llamo: ${this.nombre} ${this.apellido}`)
+    saludar(fn) {
+        var { nombre, apellido} = this 
+        // var nombre = this.nombre
+        // var apellido = this.apellido
+        console.log(`hola me llamo: ${nombre} ${apellido}`)
+        //preguntamos de esta manera si esta funcion tiene un valor es decir si nos pasaron un valor en
+        //saludar(fn) este bloque de codigo se va ejecutar es decir esta condicion va ser verdadera,
+        //si acá no nos pasaron nada entonces no se va ejecutar y esta condicion va a ser falsa.
+        //otros ejemplos (null) va a ser siempre false (0) va a ser siempre false. si tenemos algun valor que 
+        //de 0 va a dar siempre false, cualquier cosa ({}) un objeto que este vacio va a ser verdadero, 
+        //un array([]) aunque este vacio va a ser verdadero, un strigs ('')va a ser verdadro (ya un strins)
+        //vacio no. 
+        if (fn) {
+            fn(nombre, apellido) //podemos definirlo como false o no definirlo por que no es dev la persona
+        }
     }
-    soyAlto() {
-        return this.altura > 1.8
-    }
+        soyAlto() {
+            return this.altura > 1.8
+        }
 }
+
+//**ahora vamos a saludar de persona lo que vamos hacer es recibir como parametro esta funcion 
+//podemos ponerle al paremetro "el nombre que nosotros queramos fn por ejemplo, entonces lo que 
+//vamos hacer es que esta funcion sea opcional, tal vez nos la mandan o tal vez no"
+//a sahca podemos no mandarsela y borramos *responderSaludo este debe funcionar correctamente
+//se le responde a erika y arturo
+//**entonces vamos a ir al saludar y vamos a preguntar dentro de la funcion con un if si nos 
+//algo aqui dentro de la funcion puede ser que esta funcion no lo venga nada entonces
+//tenemos que preguntar si esta funcion esta definida
+
+
 //como aplicamos la herencia: ante de las llaves le vamos a poner la parte de la herencia 
 //como hacemos que herede de otra clase lo que se llama herencia acá se llama extension,
 //vamos a decir que esta clase extiende de Persona
@@ -28,23 +52,48 @@ class Desarrollador extends Persona {
         // this.altura = altura
     }
 
-    saludar() {
-        console.log(`hola me llamo ${this.nombre} ${this.apellido} y soy desarrolador/a`)
+    saludar(fn) {
+        var { nombre, apellido} = this 
+        
+        console.log(`hola me llamo ${nombre} ${apellido} y soy desarrolador/a`)
+        if (fn) {
+            fn(nombre, apellido, true) //aca aplcamos true para que nos diga yo si soy dev
+        }
     }
 
 }
+//para eso vamos a crear una funcion 
+// y despues queremos decirle a mira no sabia que eres desarrollador entonces de donde sacamos 
+//el nombre y el apellido, bueno en esta funcion no va a llegar por parametro.
+//ademas vamos a enviar un boleano que sea un true o false que no va a decir si es dev o no
+//despues vamos a preguntar si es Dev o no.
+//lo que vamos a hacer ahora es pasar nuestra funcion responderSaludo a la funcion saludar como parametro
+
+function responderSaludo (nombre, apellido, esDev){
+    console.log(`Buen día ${nombre} ${apellido}`)
+    if (esDev) {
+        console.log(`no sabia que eras desarrollador/a`)
+    }
+
+}
+
+
+var sacha = new Persona('Sacha', 'lifszyc', 1.72)
+var erika = new Persona('Erika', 'Luna', 1.65)
+var arturo = new Desarrollador('Arturo', 'Martinez', 1.90)
+
+sacha.saludar()
+erika.saludar(responderSaludo)
+arturo.saludar(responderSaludo)
+
+
+
 //nos lanza un error que nos dice debemos llamar al constructor super antes de acceder a this
 //lo que significa que no podemos utilizar this hasta que no hallamos llamado al constructor
 //De la clase padre y para llamar al constructor de la clase padre.
 
 //Para llamar al constructor de clase padre: tenemos que utilizar la palabra super a super le
 //pasamos:  super (nombre, apellido, altura)
-
-
-
-
-
-
 
 
 
@@ -72,9 +121,6 @@ class Desarrollador extends Persona {
 
 // var soyAlto = persona => persona.altura >= 1.80
 
-// var sacha = new Persona('Sacha', 'lifszyc', 1.72)
-// var erika = new Persona('Erika', 'Luna', 1.65)
-// var arturo = new Persona('Arturo', 'Martinez', 1.90)
 // arturo.soyAlto()
 // arturo.saludar()
 
