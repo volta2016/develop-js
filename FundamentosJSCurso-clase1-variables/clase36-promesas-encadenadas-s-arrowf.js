06a816843ebfe725e1aa9f0b87952af5fd8b4ba8
volta <giovoltadesign@gmail.com>
2019-08-18 19:49:16 -0300

    js36

--- FundamentosJSCurso-clase1-variables/clase36-promesas-encadenadas-s-arrowf.js
+++ FundamentosJSCurso-clase1-variables/clase36-promesas-encadenadas-s-arrowf.js
@@ -37,3 +37,63 @@ obtenerPlanetas(1)
     console.log(planeta4.name)
   })
   .catch(Error_Planeta)
+
+  //_____::::::::
+//resumen
+// const API_URL ='https://swapi.co/api/'
+// const PLANETA_URL = 'planets/:id'//apigrupo de objetos que trabajaremos
+// const OPTS = {crossDomain: true }
+
+// //armando la funcion para obtener el id del grupo de planeta que vamos a obtener
+// function obtenerPLANETA(id) {
+//     //retornamos y armamos la promesa que esya se tiene 2 estados 2 parametros
+//   return new Promise ( (resolve, reject) => {
+//     const url = `${API_URL}${PLANETA_URL.replace(':id', id)}`
+//     //lo que vamos hacer con la data que nos llegue es resolver la promesa, es decir
+//     //cuando la funcion se ejecute vamos a llamar recién al data al resolve es decir al
+//     //al planeta
+//     $
+//       .get(url, OPTS, function (data) {
+//           resolve(data)
+//       })
+//       .fail( () => reject (id) )
+// } )
+// }
+// function onError(id) {
+//   console.log(`Sucedio un error al obtener el planeta ${id}`)
+// }
+// //armamos la funcion del catch para luego invocarla despues del then
+
+// //oy a pasarle la función obtener obtenerPLANETA pasándole el id que me interesa, 
+// //no hay  ningún segundo parámetro ya que la forma de obtener el valor que esperamos 
+// //de una promesa es llamando al .then 
+// //aplicamos la funcion que se resuelve exitosamente al .then armando una arrow 
+// //fuction mas el console del persona 
+
+// //acá hay algo bastante clave que acá el primer .then va a ser el personaje1 y dentro del consolé.log va ir ${personaje1.name} y así sucesivamente para el .then del 
+// //return obtenerPersonaje(2)
+
+// //“Cuando obtengamos el return obtenerPersonaje(2) dentro del .then del personaje1" 
+// //y cuando obtengamos el valor de return obtenerPersonaje(2) este valor va a ser el 
+// //personaje de acá .then(personaje2  => {
+// //})
+
+// obtenerPLANETA(1)
+//   .then((planeta1) => {
+//     console.log(`El planeta 1 es ${planeta1.name}.`)
+//     return obtenerPLANETA(2)
+//   })
+//   .then(planeta2 => {
+//     console.log(`El planeta 2 es ${planeta2.name}.`)
+//     return obtenerPLANETA(3)
+//   })
+//   .then(planeta3 => {
+//     console.log(`El planeta 3 es ${planeta3.name}.`)
+//     return obtenerPLANETA(4)
+//   })
+//   .then(planeta4 => {
+//     console.log(`El planeta 4 es ${planeta4.name}.`)
+//   })
+//   .catch(onError)//estamos pasandoe el nombre de la funcion que se va ejecutar
+//   //Y si sucede algún error vamos vamos a utilizar la función .catch(onError)
+
